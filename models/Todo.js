e077a0935fdddef45dc94cf5ec4fd98cc4b77b60
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2022-09-02 20:48:38 -0700

    updates

--- models/Todo.js
+++ models/Todo.js
@@ -1,18 +1,36 @@
 const mongoose = require('mongoose')
 
 const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true
-  }
+ todo: {
+  type: String,
+  required: true,
+ },
+ completed: {
+  type: Boolean,
+  required: true,
+ },
+ userId: {
+  type: String,
+  required: true
+ }
 })
 
+// const TodoNtSchema = new mongoose.Schema({
+//  todo: {
+//   type: String,
+//   required: true,
+//  },
+//  completed: {
+//   type: Boolean,
+//   required: true,
+//  },
+//  userId: {
+//   type: String,
+//   required: true
+//  }
+// })
+
 module.exports = mongoose.model('Todo', TodoSchema)
+
+// module.exports = mongoose.model('Todo', TodoNtSchema)
+
