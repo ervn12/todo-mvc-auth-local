2fe5c0473dcaac852103c5f54a0aaaecd428265c
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2022-09-06 00:06:50 -0700

    Alex updated reset btn

--- public/js/main.js
+++ public/js/main.js
@@ -7,99 +7,99 @@ const todoCircle = document.querySelectorAll(".fa-circle");
 const todoCheck = document.querySelectorAll(".fa-circle-check");
 
 Array.from(deleteBtn).forEach((el) => {
-  el.addEventListener("click", deleteTodo);
+ el.addEventListener("click", deleteTodo);
 });
 
 Array.from(todoItem).forEach((el) => {
-  el.addEventListener("click", markComplete);
+ el.addEventListener("click", markComplete);
 });
 
 Array.from(todoComplete).forEach((el) => {
-  el.addEventListener("click", markIncomplete);
+ el.addEventListener("click", markIncomplete);
 });
 
 Array.from(todoCircle).forEach((el) => {
-  el.addEventListener("click", markComplete);
+ el.addEventListener("click", markComplete);
 });
 
 Array.from(todoCheck).forEach((el) => {
-  el.addEventListener("click", markIncomplete);
+ el.addEventListener("click", markIncomplete);
 });
 
 Array.from(todoReset).forEach((el) => {
-  el.addEventListener("click", resetTodo);
+ el.addEventListener("click", resetTodo);
 });
 
 async function deleteTodo() {
-  const todoId = this.parentNode.dataset.id;
-  try {
-    const response = await fetch("/todos/deleteTodo", {
-      method: "delete",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        todoIdFromJSFile: todoId,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
+ const todoId = this.parentNode.dataset.id;
+ try {
+  const response = await fetch("/todos/deleteTodo", {
+   method: "delete",
+   headers: { "Content-type": "application/json" },
+   body: JSON.stringify({
+    todoIdFromJSFile: todoId,
+   }),
+  });
+  const data = await response.json();
+  console.log(data);
+  location.reload();
+ } catch (err) {
+  console.log(err);
+ }
 }
 
 async function markComplete() {
-  const todoId = this.parentNode.dataset.id;
-  console.log(this);
-  try {
-    const response = await fetch("/todos/markComplete", {
-      method: "put",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        todoIdFromJSFile: todoId,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
+ const todoId = this.parentNode.dataset.id;
+ console.log(this);
+ try {
+  const response = await fetch("/todos/markComplete", {
+   method: "put",
+   headers: { "Content-type": "application/json" },
+   body: JSON.stringify({
+    todoIdFromJSFile: todoId,
+   }),
+  });
+  const data = await response.json();
+  console.log(data);
+  location.reload();
+ } catch (err) {
+  console.log(err);
+ }
 }
 
 async function markIncomplete() {
-  const todoId = this.parentNode.dataset.id;
-  try {
-    const response = await fetch("/todos/markIncomplete", {
-      method: "put",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        todoIdFromJSFile: todoId,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
+ const todoId = this.parentNode.dataset.id;
+ try {
+  const response = await fetch("/todos/markIncomplete", {
+   method: "put",
+   headers: { "Content-type": "application/json" },
+   body: JSON.stringify({
+    todoIdFromJSFile: todoId,
+   }),
+  });
+  const data = await response.json();
+  console.log(data);
+  location.reload();
+ } catch (err) {
+  console.log(err);
+ }
 }
 
 async function resetTodo() {
-  const todoId = this.parentNode.dataset.id;
-  console.log(this);
-  try {
-    const response = await fetch("/todos/resetTodo", {
-      method: "put",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        todoIdFromJSFile: todoId,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
+ const todoId = this.parentNode.dataset.id;
+ console.log(this);
+ try {
+  const response = await fetch("/todos/resetTodo", {
+   method: "put",
+   headers: { "Content-type": "application/json" },
+   body: JSON.stringify({
+    todoIdFromJSFile: todoId,
+   }),
+  });
+  const data = await response.json();
+  console.log(data);
+  location.reload();
+ } catch (err) {
+  console.log(err);
+ }
 }
